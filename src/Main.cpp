93994df022d60fbe3f70bc11743224cbdba1f6d6
avinal <185067@nith.ac.in>
2020-08-08 11:55:18 +0530

    Source files added

---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include "../include/code.hpp"
+#include "../include/parser.hpp"
+#include "../include/symbol_table.hpp"
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::string asmfile;
+    return 0;
+}
